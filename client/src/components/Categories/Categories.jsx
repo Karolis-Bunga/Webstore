0144f9fb925552525f02c632a8e02f98eec66c8d
Karolis-Bunga <bunga.karolis@gmail.com>
2024-05-16 23:50:41 +0300

    fixed sql version issue

---
+++ client/src/components/Categories/Categories.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import "./Categories.scss";
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <div className="col">
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/6121448/pexels-photo-6121448.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <button>
+            <Link className="link" to="/products/1">
+              Sale
+            </Link>
+          </button>
+        </div>
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/5604520/pexels-photo-5604520.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <button>
+            <Link to="/products/1" className="link">
+              Women
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col">
+        <div className="row">
+          {" "}
+          <img
+            src="https://images.pexels.com/photos/6770911/pexels-photo-6770911.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <button>
+            <Link to="/products/1" className="link">
+              New Season
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col col-l">
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              <img
+                src="https://images.pexels.com/photos/6764007/pexels-photo-6764007.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <button>
+                <Link to="/products/1" className="link">
+                  Men
+                </Link>
+              </button>
+            </div>
+          </div>
+          <div className="col">
+            <div className="row">
+              {" "}
+              <img
+                src="https://images.pexels.com/photos/2703202/pexels-photo-2703202.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <button>
+                <Link to="/products/1" className="link">
+                  Accessories
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/5878828/pexels-photo-5878828.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <button>
+            <Link to="/products/1" className="link">
+              Shoes
+            </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
